f706e0745de3bf2b55b23ddca1d676be93b8c0ff
Hajdu Csaba <kyberszittya@protonmail.ch>
2018-11-08 00:11:04 +0100

    Bugs corrected: state was invalid

--- src/gazebo_rti_state_plugin/src/plugin_main.cpp
+++ src/gazebo_rti_state_plugin/src/plugin_main.cpp
@@ -12,9 +12,12 @@ namespace gazebo
     class RtiStateGazeboPlugin: public WorldPlugin
     {
     private:
+        common::Time last_dt;
+
         std::shared_ptr<dds::domain::DomainParticipant> participant;
         std::shared_ptr<dds::topic::Topic<GazeboState::EnvironmentState> > topic;
         std::shared_ptr<dds::pub::DataWriter<GazeboState::EnvironmentState> > writer;
+
         
         event::ConnectionPtr update_event_;
         physics::WorldPtr world;
@@ -31,30 +34,38 @@ namespace gazebo
             writer = std::shared_ptr<dds::pub::DataWriter<GazeboState::EnvironmentState> >(
                 new dds::pub::DataWriter<GazeboState::EnvironmentState>(dds::pub::Publisher(*participant), *topic)
             );
+            last_dt = world->SimTime();
         }
 
         void StateUpdate()
         {
             GazeboState::EnvironmentState env_state;
-            for(unsigned int i = 0; i < world->ModelCount(); i++)
+            common::Time now = world->SimTime();
+            double dt = (now - last_dt).Double();
+            if (dt > 0.1)
             {
-                GazeboState::ObjectState objectstate;
-                auto model = world->ModelByIndex(i);
-                auto pos = model->WorldPose().Pos();
-                auto linvel = model->WorldLinearVel();
-                auto bounding_box = model->CollisionBoundingBox().Size();
-                GazeboState::Vector3 p(pos.X(), pos.Y(), pos.Z());
-                GazeboState::Vector3 v(linvel.X(), linvel.Y(), linvel.Z());
-                GazeboState::Vector3 b_box(bounding_box.X(), bounding_box.Y(), bounding_box.Z());
-                objectstate.pos(p);
-                objectstate.target(model->GetName());
-                objectstate.lin_vel(v);
-                //objectstate.bounding_box()
+                for(unsigned int i = 0; i < world->ModelCount(); i++)
+                {
+                    GazeboState::ObjectState objectstate;
+                    auto model = world->ModelByIndex(i);
+                    auto pos = model->WorldPose().Pos();
+                    auto linvel = model->WorldLinearVel();
+                    auto bounding_box = model->CollisionBoundingBox().Size();
+                    GazeboState::Vector3 p(pos.X(), pos.Y(), pos.Z());
+                    GazeboState::Vector3 v(linvel.X(), linvel.Y(), linvel.Z());
+                    GazeboState::Vector3 b_box(bounding_box.X(), bounding_box.Y(), bounding_box.Z());
+                    objectstate.pos(p);
+                    objectstate.target(model->GetName());
+                    objectstate.lin_vel(v);
+                    //objectstate.bounding_box()
 
-                env_state.objects().push_back(objectstate);
+                    env_state.objects().push_back(objectstate);
+                }
+                env_state.world_name(world->Name());
+                writer->write(env_state);
+                last_dt = now;
             }
-            env_state.world_name(world->Name());
-            writer->write(env_state);
+            
         }
 
     };
